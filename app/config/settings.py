a36cfc539d3e3cd9de713dde13a10e1c56722a19
Marco Kang <kmnkit@gmail.com>
2021-03-23 20:07:13 +0900

    added recipe images upload feature

--- app/config/settings.py
+++ app/config/settings.py
@@ -116,5 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 AUTH_USER_MODEL = 'users.User'
--- app/recipes/serializers.py
+++ app/recipes/serializers.py
@@ -47,3 +47,11 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(sz.ModelSerializer):
+    """Serializer for uploading images to recipes"""
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
